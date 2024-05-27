23801ebc558730b6c3835b3e728966f1c2e987dc
Greedywebminer <168652146+Greedywebminer@users.noreply.github.com>
2024-05-26 21:17:40 -0800

    home.sh

--- home.sh
+++ home.sh
@@ -1,4 +1,11 @@
-termux-wake-lock
-ssh
-cd ccminer && keyevent 66
-./start.sh && keyevent 66
+adb shell "input text 'nano \$HOME/.bashrc'"
+adb shell input keyevent 66  # Press Enter to open nano
+
+adb shell "input text 'termux-wake-lock'"
+adb shell input keyevent 66  # Press Enter to enter the command
+
+adb shell "input text 'sshd'"
+adb shell input keyevent 66  # Press Enter to enter the command
+
+adb shell "input text 'cd ccminer && ./start.sh'"
+adb shell input keyevent 66  # Press Enter to enter the command
